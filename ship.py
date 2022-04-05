9fe4af97c843cfe7cdbdae90fe749a92140fa651
mephishin <epishin1anatoly@gmail.com>
2022-04-05 20:30:47 +0300

    init

---
+++ ship.py
@@ -0,0 +1,36 @@
+import pygame
+
+
+class Ship():
+    """Class for controlling a ship"""
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        # load ship's image and get rectangle
+        self.image = pygame.image.load('images for ai/ship.bmp')
+        self.rect = self.image.get_rect()
+        # each new ship appeared at the bottom of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+        # moving flag
+        self.moving_right = False
+        self.moving_left = False
+
+        self.x = float(self.rect.x)
+
+    def blitme(self):
+        """Draw ship in current position"""
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Updating ships position"""
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        self.rect.x = self.x
+
+    def center_ship(self):
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
